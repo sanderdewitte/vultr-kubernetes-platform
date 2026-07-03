06e7021010770c50e93b80ce40da2bb550004b5d
sdewitte <github@sanderdewitte.com>
2026-07-03 18:42:43 +0200

    Updated Pulumi infrastructure's __main__.py
    Added domain application Argo CD applications, updated docstring accordingly

--- infra/__main__.py
+++ infra/__main__.py
@@ -2,9 +2,10 @@
 """
 Pulumi entrypoint for the Vultr Kubernetes platform.
 
-This file orchestrates resource creation by calling individual
-modules for configuration, providers, the Kubernetes cluster,
-worker node lookup, DNS, secrets, platform database and outputs.
+This file orchestrates resource creation by calling individual modules
+for configuration, providers, the Kubernetes cluster, worker node lookup,
+DNS, secrets, the platform database, domain application databases,
+Argo CD application definitions and outputs.
 """
 
 # Imports
@@ -14,8 +15,9 @@ from cluster import create_cluster
 from worker_node import get_first_worker_public_ip
 from dns import create_dns_records
 from secure import create_kubernetes_secrets
-from database import create_postgresql_namespace, create_postgresql_cluster, create_domain_application_databases
 from constants import POSTGRESQL_SUPERUSER_SECRET_KEY, POSTGRESQL_APP_SECRET_KEY
+from database import create_postgresql_namespace, create_postgresql_cluster, create_domain_application_databases
+from apps import create_domain_application_argocd_applications
 from outputs import export_outputs
 
 # Configuration
@@ -48,6 +50,13 @@ domain_application_databases = create_domain_application_databases(
     settings=settings,
     kubernetes_provider=kubernetes_provider,
     postgresql_cluster=postgresql_cluster,
+)
+
+# Domain application Argo CD Applications
+domain_application_argocd_applications = create_domain_application_argocd_applications(
+    settings=settings,
+    kubernetes_provider=kubernetes_provider,
+    domain_application_namespaces=kubernetes_secrets["domain_application_namespaces"],
 )
 
 # Outputs
